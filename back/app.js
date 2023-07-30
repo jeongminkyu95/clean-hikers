bc79c9453acea7f60a01af01cb5335c93a13e00a
minkyu <tour00389@gmail.com>
2023-07-30 23:36:16 +0900

    feat : Set server port as environment variable

--- back/app.js
+++ back/app.js
@@ -9,20 +9,26 @@ import { mainRouter } from "./src/main/mainRouter.js";
 import { mountainRouter } from "./src/mountain/mountainRouter.js";
 import { locationRouter } from "./src/community_location/locationRouter.js";
 import { errorMiddleware } from "./src/middlewares/errorMiddleware.js";
+
 const app = express();
 
+// CORS 에러 방지
 app.use(cors());
-app.use(express.json({
-    limit : '1mb'
-}));
-app.use(express.urlencoded({ 
-    limit : '1mb',
-    extended: false }));
 
-const port = 5000;
+app.use(
+  express.json({
+    limit: "1mb",
+  })
+);
+app.use(
+  express.urlencoded({
+    limit: "1mb",
+    extended: false,
+  })
+);
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+  res.send("hello world");
 });
 
 //라우터 추가
@@ -36,6 +42,8 @@ app.use("/mountain", mountainRouter);
 
 app.use(errorMiddleware);
 
+const port = process.env.SERVER_PORT || 5000;
+
 app.listen(port, () => {
-    console.log(`${port}에 연결되었습니다`);
+  console.log(`${port}에 연결되었습니다`);
 });

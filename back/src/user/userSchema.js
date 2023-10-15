5600b3d214104b4df0532ab2a74b7229be14b422
minkyu <tour00389@gmail.com>
2023-10-15 14:18:05 +0900

    :recycle: refactor `[userSchema.js]` : Make nickname, id, and email fields unique in user schema

--- back/src/user/userSchema.js
+++ back/src/user/userSchema.js
@@ -1,17 +1,20 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema(
   {
     id: {
       type: String,
+      unique: true,
       required: true,
     },
     email: {
       type: String,
+      unique: true,
       required: true,
     },
     nickname: {
       type: String,
+      unique: true,
       required: true,
     },
     password: {
@@ -23,15 +26,15 @@ const UserSchema = new Schema(
       required: false,
       default: "설명이 아직 없습니다. 추가해 주세요.",
     },
-    deleted : {
-      type : Boolean,
-      required : true,
-      default : false
+    deleted: {
+      type: Boolean,
+      required: true,
+      default: false,
     },
-    defaultImage:{
-      type : String,
-      default : null
-    }
+    defaultImage: {
+      type: String,
+      default: null,
+    },
   },
   {
     timestamps: true,
@@ -40,4 +43,4 @@ const UserSchema = new Schema(
 
 const UserModel = model("User", UserSchema);
 
-export  {UserModel}
+export { UserModel };

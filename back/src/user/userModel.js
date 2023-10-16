164fc340f9e7bdf945f3c32745890d6f2a668756
minkyu <tour00389@gmail.com>
2023-10-16 15:26:14 +0900

    ♻️ refactor `[userModel.js]` : Simplify object filter and update in user model methods

--- back/src/user/userModel.js
+++ back/src/user/userModel.js
@@ -15,9 +15,6 @@ class User {
   // 유저 조회 by id
   static async findByID(id) {
     const findUser = await UserModel.findOne({ id: id });
-    if (!findUser) {
-      throw new Error("유저가 존재하지 않습니다.");
-    }
     return findUser;
   }
 
@@ -29,58 +26,38 @@ class User {
 
   // 유저 조회 후 닉네임 변경
   static async findByIDandChangeNickname(userID, changingData) {
-    const filter = {
-      id: userID,
-    };
-    const update = {
-      nickname: changingData,
-    };
-    const option = { returnOriginal: false };
     const changedUser = await UserModel.findOneAndUpdate(
-      filter,
-      update,
-      option
+      { id: userID },
+      { nickname: changingData },
+      { returnOriginal: false }
     );
     return changedUser;
   }
 
   // 유저 조회 후 비밀번호 변경
   static async findByIDandChangePassword(userID, changingData) {
-    const filter = {
-      id: userID,
-    };
-    const update = {
-      password: changingData,
-    };
-
-    const changedUser = await UserModel.findOneAndUpdate(filter, update);
-    console.log("password changed : ", changedUser.password);
+    const changedUser = await UserModel.findOneAndUpdate(
+      { id: userID },
+      { password: changingData }
+    );
     return changedUser;
   }
 
   // 유저 조회 후 사진 변경
   static async findByIDandChangePhoto(userID, image) {
-    const filter = {
-      id: userID,
-    };
-    const update = {
-      defaultImage: image,
-    };
-
-    const changedImage = await UserModel.findOneAndUpdate(filter, update);
-    console.log("image changed", changedImage.defaultImage);
+    const changedImage = await UserModel.findOneAndUpdate(
+      { id: userID },
+      { defaultImage: image }
+    );
     return changedImage;
   }
 
   // 유저 조회 후 삭제
   static async findByIDandDeleteUser(id) {
-    const filter = {
-      id: id,
-    };
-    const deleted = {
-      deleted: true,
-    };
-    const deletedUser = await UserModel.findOneAndUpdate(filter, deleted);
+    const deletedUser = await UserModel.findOneAndUpdate(
+      { id: id },
+      { deleted: true }
+    );
     return deletedUser;
   }
 }

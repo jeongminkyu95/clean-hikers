b296e62390ca6feae100589f9eaf3862e126a063
minkyu <tour00389@gmail.com>
2023-10-18 16:16:33 +0900

    ✨ feat `[CommunityNav.js]` : Add feature to fetch new posts when tab changes
    - Removed the logic to filter existing posts based on tabs.
    - Implemented new behavior to make a fresh API request with the changed tab, immediately resetting pageNum to 1.

--- front/src/components/community/component/CommunityNav.js
+++ front/src/components/community/component/CommunityNav.js
@@ -1,18 +1,14 @@
-import { Card, Col, Row, Select } from "antd";
-import React, { useEffect, useState } from "react";
+import { Row } from "antd";
+import React from "react";
 import {
   CommunityNavStyled,
   CommunityNavCol,
-  SelectStyled,
 } from "../styledComponents/CommunityNavStyled";
 import CommunityItem from "./CommunityItem";
-import initialState from "./data";
-import * as api from "../../../api/api";
 
-const { Option } = Select;
 const tabList = [
   {
-    key: "allPost",
+    key: "allPosts",
     tab: "전체",
   },
   {
@@ -29,43 +25,15 @@ const tabList = [
   },
 ];
 
-function CommunityNav({ setPosts, posts }) {
-  const [activeTabKey1, setActiveTabKey1] = useState("allPost");
-  const [tabs, setTabs] = useState(initialState.inputs.state);
-  const [stateNum, setStateNum] = useState(1);
-  const [station, setStation] = useState();
+function CommunityNav({ posts, onChange, setPageNum }) {
+  const content = (
+    <p>
+      {posts.map((post) => (
+        <CommunityItem key={post.no} post={post} />
+      ))}
+    </p>
+  );
 
-  // 데이터 어떻게 받아오실지 몰라서 일단 요렇게 남겨놓아요!
-  const mountainData = [
-    { name: "덕유산", id: "1" },
-    { name: "지리산", id: "2" },
-  ];
-
-  const filterItem = posts
-    .filter((post) => post.station === tabs)
-    .map((post) => <CommunityItem key={post.no} post={post} />);
-
-  const contentList = {
-    allPost: (
-      <p>
-        {posts.map((post) => (
-          <CommunityItem
-            key={post.no}
-            posts={posts}
-            setPosts={setPosts}
-            post={post}
-          />
-        ))}
-      </p>
-    ),
-    클린후기: <div>{filterItem}</div>,
-    모집중: <div>{filterItem}</div>,
-    모집완료: <p>{filterItem}</p>,
-  };
-
-  const onTab1Change = (key) => {
-    setActiveTabKey1(key);
-  };
   return (
     <>
       <Row justify="center">
@@ -75,20 +43,13 @@ function CommunityNav({ setPosts, posts }) {
               width: "100%",
             }}
             tabList={tabList}
-            activeTabKey={activeTabKey1}
+            activeTabKey={tabList}
             onTabChange={(key) => {
-              onTab1Change(key);
-              setTabs(key);
+              onChange(key);
+              setPageNum(1);
             }}
-            // tabBarExtraContent={
-            //   <SelectStyled bordered={false} placeholder="산 전체">
-            //     {mountainData.map((mountain) => {
-            //       return <Option key={mountain.name}>{mountain.name}</Option>;
-            //     })}
-            //   </SelectStyled>
-            // }
           >
-            {contentList[activeTabKey1]}
+            {content}
           </CommunityNavStyled>
         </CommunityNavCol>
       </Row>

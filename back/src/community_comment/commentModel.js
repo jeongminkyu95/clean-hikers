042fded5595e65aa54a89e572fcf492f68617a91
minkyu <tour00389@gmail.com>
2023-10-19 22:58:01 +0900

    ♻️ refactor `[commentModel.js]` : Remove comment modification and add function for updating comment author's nickname on user nickname change
    - Removed the function that modifies comments.
    - Added a new function that updates all comments' author nickname when a user's nickname changes.

--- back/src/community_comment/commentModel.js
+++ back/src/community_comment/commentModel.js
@@ -1,51 +1,26 @@
 import { CommentModel } from "../community_comment/commentSchema.js";
 
 class Comment {
-  static async create({ newComment }) {
+  // 댓글 생성
+  static async create(newComment) {
     return await CommentModel.create(newComment);
   }
 
-  static async findByPostId({ post_id }) {
+  // 특정 게시글 댓글 조회
+  static async findByPostId(post_id) {
     return await CommentModel.find({ post_id: post_id });
   }
 
-  static async findByCommentId({ comment_id }) {
-    const comment = await CommentModel.findOne({ comment_id: comment_id });
-
-    if (!comment) {
-      throw new Error("댓글이 존재하지 않습니다.");
-    }
-
-    return comment;
+  // 댓글의 작성자 닉네임 일괄 수정
+  static async updateMany({ user_id, toUpdate }) {
+    return await CommentModel.updateMany({ user_id: user_id }, toUpdate);
   }
 
-  static async findAll() {
-    return await CommentModel.find({});
-  }
-
-  static async update({ comment_id, fieldToUpdate, newValue }) {
-    const filter = { comment_id: comment_id };
-    const update = { [fieldToUpdate]: newValue };
-    const option = { returnOriginal: false };
-
-    const updatedComment = await CommentModel.findOneAndUpdate(
-      filter,
-      update,
-      option
-    );
-    return updatedComment;
-  }
-
-  static async deleteByCommentId({ comment_id }) {
-    const comment = await CommentModel.deleteOne({
+  // 댓글 삭제
+  static async deleteByCommentId(comment_id) {
+    return await CommentModel.deleteOne({
       comment_id: comment_id,
     });
-
-    if (comment.deletedCount === 0) {
-      throw new Error("해당 댓글을 삭제할 수 없습니다.");
-    }
-
-    return comment;
   }
 }
 

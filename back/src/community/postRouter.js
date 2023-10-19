19e09026d579e420951db69ed8ab0716bcc47d20
minkyu <tour00389@gmail.com>
2023-10-19 16:59:01 +0900

    ♻️ refactor `[postRouter.js]` : Enhance code readability and add pagination features
    - Improve the readability of the code by adding more descriptive comments and renaming variables for clarity
    - Add pagination to 'getUserPosts' and 'getAllPosts' functions to limit the number of posts returned
    - Change error handling strategy by removing explicit ErrorMessage function calls and relying on middleware error handling instead
    - Modify 'changeNicknamePost' function to not return a value, reflecting that its purpose is a side effect (updating posts)

--- back/src/community/postRouter.js
+++ back/src/community/postRouter.js
@@ -1,130 +1,121 @@
 import is from "@sindresorhus/is";
 import { Router } from "express";
-import { postService } from "./communityService.js";
-import { ErrorMessage } from "../middlewares/errorMiddleware.js";
+import { loginRequired } from "../middlewares/loginRequired.js";
+import { NotLoginUser } from "../utils/CustomError.js";
+import { postService } from "./postService.js";
+import { throwIfNoData } from "../utils/throwIfNoData.js";
 
 const postRouter = Router();
 
+// 수정, 삭제시 로그인 유저와 해당 게시글의 작성 유저가 동일한지 검증필요.
+// loginREquired필요. but 현재 클라이언트에서 전송 x
+
 // 게시글 추가
 postRouter.post("/post", async function (req, res, next) {
-    try {
-        if (is.emptyObject(req.body)) {
-            throw new Error(
-                "headers의 Content-Type을 application/json으로 설정해주세요"
-            );
-        }
+  try {
+    // 유효성 검사 로직 추가 예정
+    // throwIfNoData(req.loginedUser.id == req.body.user_id, NotLoginUser);
 
-        const newPost = await postService.addPost(req.body);
-
-        ErrorMessage(newPost);
-        res.status(201).json(newPost);
-    } catch (error) {
-        next(error);
+    // 유효성 검사 : req.body가 비어있는지 확인
+    if (is.emptyObject(req.body)) {
+      throw new Error(
+        "headers의 Content-Type을 application/json으로 설정해주세요"
+      );
     }
+
+    const newPost = await postService.addPost(req.body);
+    res.status(201).json(newPost);
+  } catch (error) {
+    next(error);
+  }
 });
 
-//해당 유저의 게시글 조회 (페이지네이션 적용)
+// 마이페이지 작성 게시글 조회 (페이지네이션 5개씩)
 postRouter.get("/posts/:userId", async function (req, res, next) {
-    try {
-        const user_id = req.params.userId;
-        const pagination = req.query;
-        const posts = await postService.getUserPosts({
-            user_id,
-            pagination,
-        });
+  try {
+    const user_id = req.params.userId;
+    const pagination = req.query;
 
-        ErrorMessage(posts);
-        res.status(200).send(posts);
-    } catch (error) {
-        next(error);
-    }
+    // 사용자의 모든 게시물을 조회하고 페이지네이션 적용
+    const posts = await postService.getUserPosts({
+      user_id,
+      pagination,
+    });
+
+    res.status(200).send(posts);
+  } catch (error) {
+    next(error);
+  }
 });
 
-// //모든 게시글 조회
-// postRouter.get("/postlist", async function (req, res, next) {
-//     try {
-//         const posts = await postService.getAllPosts();
-
-//         res.status(200).send(posts);
-//     } catch (error) {
-//         next(error);
-//     }
-// });
-
-//모든 게시글 조회 (5개씩 출력) (모집중, 모집후기, 클린후기 , 산에 따라 분류해서 출력)
+//모든 게시글 조회 (station별로 5개씩 출력)
 postRouter.get("/postlist", async function (req, res, next) {
-    try {
-        const send = req.query;
-        const postList = await postService.getAllPosts(send);
+  try {
+    const { page, perPage, station } = req.query;
 
-        res.status(200).send(postList);
-    } catch (error) {
-        next(error);
-    }
+    // 지정된 station에 따라 모든 게시물을 조회하고 페이지네이션 적용
+    const postList = await postService.getAllPosts({ page, perPage, station });
+    res.status(200).send(postList);
+  } catch (error) {
+    next(error);
+  }
 });
 
-//해당 게시글 조회
+//특정 게시글 상세 조회
 postRouter.get("/postsDetail/:postId", async function (req, res, next) {
-    try {
-        const post_id = req.params.postId;
+  try {
+    const posts = await postService.getPostDetail(req.params.postId);
 
-        const posts = await postService.getAPosts({ post_id });
-
-        res.status(200).send(posts);
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).send(posts);
+  } catch (error) {
+    next(error);
+  }
 });
 
 //게시글 수정
 postRouter.put("/posts/:postId", async function (req, res, next) {
-    try {
-        const post_id = req.params.postId;
+  try {
+    const post_id = req.params.postId;
+    const toUpdate = req.body;
 
-        const toUpdate = req.body;
-        const updatedPost = await postService.setPost({
-            post_id,
-            toUpdate,
-        });
+    const updatedPost = await postService.setPost({
+      post_id,
+      toUpdate,
+    });
 
-        ErrorMessage(updatedPost);
-        res.status(200).json(updatedPost);
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
 });
 
-//유저정보 수정시 게시글 수정
+//유저 닉네임 수정시 게시글 작성자 닉네임 수정
 postRouter.put("/users/:id", async function (req, res, next) {
-    try {
-        const user_id = req.params.id;
-        const toUpdate = req.query;
+  try {
+    const user_id = req.params.id;
+    const toUpdate = req.query;
 
-        const posts = await postService.getPosts({ user_id });
+    // 사용자의 닉네임 변경 후 그에 따라 해당 사용자가 작성한 모든 게시물의 닉네임도 업데이트함.
+    await postService.changeNicknamePost({
+      user_id,
+      toUpdate,
+    });
 
-        const newPosts = await postService.changeNicknamePost({
-            posts,
-            toUpdate,
-        });
-
-        ErrorMessage(newPosts);
-        res.status(200).json(newPosts);
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).send("ok");
+  } catch (error) {
+    next(error);
+  }
 });
 
 // 게시글 삭제
 postRouter.delete("/posts/:postId", async function (req, res, next) {
-    try {
-        const post_id = req.params.postId;
-        const posts = await postService.deletePost({ post_id });
+  try {
+    const posts = await postService.deletePost(req.params.postId);
 
-        ErrorMessage(posts);
-        res.send("삭제가 완료되었습니다.");
-    } catch (error) {
-        next(error);
-    }
+    res.send("삭제가 완료되었습니다.");
+  } catch (error) {
+    next(error);
+  }
 });
 
 export { postRouter };

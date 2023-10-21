3a2e5d86ffe64a98ea3e5b3f6e9cd83516b9e367
minkyu <tour00389@gmail.com>
2023-10-21 23:05:53 +0900

    ♻️ refactor `[postSchema.js]` : Update field names for better clarity and remove 'comment' field
    - Renamed 'person' field to 'participants' for better understanding of its purpose.
    - Renamed 'personnel' field to 'participantsLimit' to clearly indicate its function.
    - Removed the 'comment' field as comments are now managed in a separate collection.

--- back/src/community/postSchema.js
+++ back/src/community/postSchema.js
@@ -1,65 +1,59 @@
 import { Schema, model } from "mongoose";
 
 const PostSchema = new Schema(
-    {
-        user_id: {
-            type: String,
-            required: true,
-        },
-        post_id: {
-            type: String,
-            required: true,
-        },
-        nickname: {
-            type: String,
-            required: true,
-        },
-        title: {
-            type: String,
-            required: true,
-            default: "제목을 입력해 주세요.",
-        },
-        description: {
-            type: String,
-            required: false,
-            default: "내용을 입력하세요",
-        },
-        date: {
-            type: Date,
-            required: true,
-            default: Date.now,
-        },
-        location: {
-            type: Object,
-            required: false,
-        },
-        person: [
-            {
-                type: Object,
-                // ref: "Person",
-            },
-        ],
-        count: {
-            type: Number,
-            required: false,
-            default: 0,
-        },
-        personnel: {
-            type: Number,
-        },
-        station: {
-            type: String,
-        },
-        comment: [
-            {
-                type: Object,
-                // ref: "Comment",
-            },
-        ],
+  {
+    user_id: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    post_id: {
+      type: String,
+      required: true,
+    },
+    nickname: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      default: "제목을 입력해 주세요.",
+    },
+    description: {
+      type: String,
+      required: false,
+      default: "내용을 입력하세요",
+    },
+    date: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+    location: {
+      type: Object,
+      required: false,
+    },
+    participants: [
+      {
+        type: Object,
+        // ref: "Person",
+      },
+    ],
+    count: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    participantsLimit: {
+      type: Number,
+    },
+    station: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const PostModel = model("Post", PostSchema);

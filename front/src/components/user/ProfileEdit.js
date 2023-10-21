e8751d6a2e38f21bc0230515f1f42c265f96981c
minkyu <tour00389@gmail.com>
2023-10-21 22:11:34 +0900

    :alien: fix `[ProfileEdit.js]` : Update API endpoint due to external changes
    - Changed the API endpoint from 'community/posts/comment/byUser/${user.id}' to 'community/posts/comments/byUser/${user.id}' in the changeNickname function due to changes in the external API.

--- front/src/components/user/ProfileEdit.js
+++ front/src/components/user/ProfileEdit.js
@@ -95,7 +95,7 @@ function ProfileEdit({ setIsEdit, user, setUser }) {
         await api.put(`community/posts/byUser/${user.id}`, {
           nickname: user.nickname,
         });
-        await api.put(`community/posts/comment/byUser/${user.id}`, {
+        await api.put(`community/posts/comments/byUser/${user.id}`, {
           nickname: user.nickname,
         });
         enterLoading(0);

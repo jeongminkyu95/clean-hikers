5b107ca5c50f4fa161ff8b10560ff337f7b9246b
minkyu <tour00389@gmail.com>
2023-10-23 17:48:01 +0900

    🔒 refactor `[perseonRouter.js]` : Apply loginRequired middleware.
    - Applied loginRequired middleware to ensure that only logged-in users can participate.

--- back/src/community_person/personRouter.js
+++ back/src/community_person/personRouter.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { personService } from "./personService.js";
+import { loginRequired } from "../middlewares/loginRequired.js";
 
 const personRouter = Router();
 
 // 모집인원 추가
 personRouter.post(
   "/posts/:post_id/participants",
+  loginRequired,
   async function (req, res, next) {
     try {
       const newPerson = await personService.addPerson(req.body);

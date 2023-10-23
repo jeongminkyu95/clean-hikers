c432a6219bcc3322a7624d5d58130d3b21cc5341
minkyu <tour00389@gmail.com>
2023-10-23 18:16:52 +0900

    :wrench: fix `[loginRequired.js]`: Parse token from Authorization header with Bearer
    - Updated the way to parse the token from the Authorization header.
    - Separate 'Bearer' and the actual token due to the client's modification of including 'Bearer' in the sent token.

--- back/src/middlewares/loginRequired.js
+++ back/src/middlewares/loginRequired.js
@@ -7,16 +7,15 @@ const loginRequired = (req, res, next) => {
     // 토큰이 없다면 로그인이 필요한 서비스라는 에러메시지 throw
     throwIfNoData(req.headers.authorization, NotLoginUser);
 
-    if (req.headers.authorization === process.env.AdminAuthorization) {
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (token === process.env.AdminAuthorization) {
       // test용 admin계정.
       req.loginedUser = { id: process.env.Admin };
       return next();
     }
 
-    req.loginedUser = jwt.verify(
-      req.headers.authorization,
-      process.env.JWT_SECRET_KEY
-    );
+    req.loginedUser = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
     // 토큰과 user_id 일치 여부 체크
     if (req.params.userId) {

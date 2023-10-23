ded226677a2a4391b0ec86dcb89b2a93c19a36a3
minkyu <tour00389@gmail.com>
2023-10-23 17:03:41 +0900

    🔒 refactor `[loginRequired.js]` : Add token absence handling and user ID verification
    - Added exception handling for cases when the token is not provided.
    - Added logic to verify that the user ID in the request matches the one decoded from the token for security enhancement.

--- back/src/middlewares/loginRequired.js
+++ back/src/middlewares/loginRequired.js
@@ -1,16 +1,34 @@
 import jwt from "jsonwebtoken";
+import { NotLoginUser, TokenNotFoundError } from "../utils/CustomError.js";
+import { throwIfNoData } from "../utils/throwIfNoData.js";
 
 const loginRequired = (req, res, next) => {
   try {
-    // test용 admin계정.
+    // 토큰이 없다면 로그인이 필요한 서비스라는 에러메시지 throw
+    throwIfNoData(req.headers.authorization, NotLoginUser);
+
     if (req.headers.authorization === process.env.AdminAuthorization) {
+      // test용 admin계정.
       req.loginedUser = { id: process.env.Admin };
       return next();
     }
+
     req.loginedUser = jwt.verify(
       req.headers.authorization,
       process.env.JWT_SECRET_KEY
     );
+
+    // 토큰과 user_id 일치 여부 체크
+    if (req.params.userId) {
+      if (req.params.userId !== req.loginedUser.id) {
+        throw new NotLoginUser(); // 에러 내용 바꿀 예정
+      }
+    } else if (req.body.user_id) {
+      if (req.body.user_id !== req.loginedUser.id) {
+        throw new NotLoginUser();
+      }
+    }
+
     return next();
   } catch (error) {
     next(error);

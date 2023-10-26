c92e1e6070177af8d0d81c69550c37d15b4c14df
minkyu <tour00389@gmail.com>
2023-10-26 15:08:01 +0900

    ⚡️ perf `[mountainSchema.js]` : Add ascending index to 'name' field for optimized query performance
    - Added an ascending index to the 'name' field to optimize the performance of queries when retrieving mountains in ascending order by name.
    - Improved the speed and efficiency of database operations.

--- back/src/mountain/mountainSchema.js
+++ back/src/mountain/mountainSchema.js
@@ -1,46 +1,47 @@
 import { Schema, model } from "mongoose";
 
 const MountainSchema = new Schema(
-    {
-        id: {
-            type: String,
-            required: true,
-        },
-        name: {
-            type: String,
-            required: true,
-        },
-        trash: {
-            type: String,
-            required: true,
-        },
-
-        latitude: {
-            type: String,
-            required: true,
-        },
-        longitude: {
-            type: String,
-            required: true,
-        },
-        address: {
-            type: String,
-            required: true,
-        },
-        difficulty: {
-            type: String,
-            required: true,
-        },
-        image: {
-            type: String,
-            default: null,
-        },
+  {
+    id: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-    }
+    name: {
+      type: String,
+      required: true,
+    },
+    trash: {
+      type: String,
+      required: true,
+    },
+
+    latitude: {
+      type: String,
+      required: true,
+    },
+    longitude: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    difficulty: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
+MountainSchema.index({ name: 1 });
 const MountainModel = model("Mountain", MountainSchema);
 
 export { MountainModel };

d36f13ffc00c92ce18c92350018abdad3256e0e4
minkyu <tour00389@gmail.com>
2023-12-07 17:30:08 +0900

    🔥 remove `[LocationService.js]` : Remove unnecessary assignment to variable "data"
    - Removed the unnecessary assignment to the variable "data"

--- back/src/community_location/LocationService.js
+++ back/src/community_location/LocationService.js
@@ -4,10 +4,6 @@ class LocationService {
   static async getData() {
     return await Mountain.findData();
   }
-
-  static async detailLocation() {
-    const data = await Mountain.findData();
-  }
 }
 
 export { LocationService };

bb1995ef73434ce6bcf1e5e221e001af77c432be
minkyu <tour00389@gmail.com>
2023-12-10 02:45:49 +0900

    ♻️ refactor `[MountainService.js]` : Remove try-catch for consistent error handling, optimize sorting and slicing in readDataBasedGarbage method.
    - Removed unnecessary nested await statement and simplified the code structure.

--- back/src/mountain/MountainService.js
+++ back/src/mountain/MountainService.js
@@ -2,63 +2,53 @@ import { Mountain } from "../mongoDB/index.js";
 
 class MountainService {
   static async readData(query) {
-    try {
-      const mountainName = query.mountain || null;
-      const mountainLocation = query.location || null;
-      const mountainDifficulty = query.level || null;
-      const totalData = await Mountain.findData(
-        mountainName,
-        mountainLocation,
-        mountainDifficulty
-      );
+    const mountainName = query.mountain || null;
+    const mountainLocation = query.location || null;
+    const mountainDifficulty = query.level || null;
+    const totalData = await Mountain.findData(
+      mountainName,
+      mountainLocation,
+      mountainDifficulty
+    );
 
-      let page = Number(query.currentPage || 1);
-      const perPage = 5;
+    let page = Number(query.currentPage || 1);
+    const perPage = 5;
 
-      const total = totalData.length;
+    const total = totalData.length;
 
-      const totalPage = Math.ceil(total / perPage);
-      if (page > totalPage) {
-        page = totalPage;
-      }
-      const currentPageList = totalData.slice(
-        perPage * (page - 1),
-        perPage * page
-      );
-      const result = {};
-
-      for (let i = 0; i < currentPageList.length; i++) {
-        if (currentPageList[i] == undefined) {
-          result.mountain = currentPageList.slice(0, i);
-          break;
-        }
-        if (i == currentPageList.length - 1) {
-          result.mountain = currentPageList;
-        }
-      }
-      result["maxPage"] = totalPage;
-      return result;
-    } catch (error) {
-      console.error(error);
-      throw error;
+    const totalPage = Math.ceil(total / perPage);
+    if (page > totalPage) {
+      page = totalPage;
     }
+    const currentPageList = totalData.slice(
+      perPage * (page - 1),
+      perPage * page
+    );
+    const result = {};
+
+    for (let i = 0; i < currentPageList.length; i++) {
+      if (currentPageList[i] == undefined) {
+        result.mountain = currentPageList.slice(0, i);
+        break;
+      }
+      if (i == currentPageList.length - 1) {
+        result.mountain = currentPageList;
+      }
+    }
+    result["maxPage"] = totalPage;
+    return result;
   }
 
   static async readDataBasedGarbage() {
-    try {
-      const totalData = await (
-        await Mountain.findData()
-      ).sort(function (a, b) {
+    const totalData = await Mountain.findData();
+
+    const result = totalData
+      .sort(function (a, b) {
         return -(a.trash - b.trash);
-      });
+      })
+      .slice(0, 4);
 
-      const result = totalData.slice(0, 4);
-
-      return result;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+    return result;
   }
 }
 
